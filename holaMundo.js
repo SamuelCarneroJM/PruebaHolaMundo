8d9b0fac7b16fc499c87ff0c66eb657050c02dcb
SamuelCarneroJM <samuel.carnero.jimenez@iesjulianmarias.es>
2026-09-16 09:08:46 +0200

    Clean up console log statements in holaMundo.js
    Removed duplicate console log statements.

--- holaMundo.js
+++ holaMundo.js
@@ -1,6 +1,2 @@
 console.log("Hola, mundos");
 console.log("Hola, mundos2");
-console.log("Hola, mundos");
-console.log("Hola, mundos4");
-console.log("Hola, mundos5");
-console.log("Hola, mundos5");
